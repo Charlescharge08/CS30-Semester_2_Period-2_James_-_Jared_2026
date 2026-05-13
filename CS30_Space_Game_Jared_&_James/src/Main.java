aeb0e9d7b8dcf2c207bb9bfbe164f0d7d99420b5
James Peterson <145619587+Charlescharge08@users.noreply.github.com>
2026-05-13 12:29:07 -0600

    Made it is that insted of using the VScode terminal it will oppen the host sytems terminal and print ouputs and accept inputs from the host terminal

--- CS30_Space_Game_Jared_&_James/src/Main.java
+++ CS30_Space_Game_Jared_&_James/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        if (args.length == 0 || !args[0].equals("opened")) {
+
+            Terminal_Oppener.main(new String[0]);
+            // stops the vscode terminal from opening
+            return;
+        }
+        runApp();
+        System.exit(0);
+
+    }
+
+     private static void runApp() {
+        //Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("This is the command terminal for the game.");
+
+        System.out.print("Enter a word of your choice: ");
+        String input = scanner.nextLine();
+
+        System.out.println("You typed: " + input);
+        System.out.println("Press enter to exit...");
+        scanner.nextLine();
+        //ends
+        scanner.close();
     }
 }
--- CS30_Space_Game_Jared_&_James/src/Terminal_Oppener.java
+++ CS30_Space_Game_Jared_&_James/src/Terminal_Oppener.java
@@ -1,4 +1,35 @@
+import java.io.*;
 public class Terminal_Oppener {
+    public static void main(String[] args) {
+        // this will pervent the program from oppening multiple terminals
+        if (args.length > 0 && args[0].equals("opened")) {
+            
+            return;
+        }
+        // Detects the operating system
+        String systemType = System.getProperty("os.name").toLowerCase();
+        try{
+            //java sdk location
+            String javaSDK = System.getProperty("java.home") + "/bin/java";
+            //Curent program Location
+            String classPath = System.getProperty("java.class.path");
+            //comand to lanuch the terminal
+            String command =  "\"" + javaSDK + "\" -cp \"" + classPath + "\" Main opened";
 
-    
+            // Check if the operating system is Windows
+            if (systemType.contains("win")) {
+                 new ProcessBuilder( "cmd", "/c", "start", "cmd", "/c", command).start();
+            } else if (systemType.contains("mac")) {
+                new ProcessBuilder("osascript", "-e", "tell application \"Terminal\" to do script \"" + command.replace("\"", "\\\"") + "; exit\"").start();
+            }else {
+                new ProcessBuilder("sh", "-c", "gnome-terminal -- " + command + " || xterm -e " + command ).start();
+            }   
+        } catch (IOException error) {
+            System.out.println("Could not open terminal.");
+            error.printStackTrace();
+        }
+            
+    }    
+
+
 }
