d7a7f4b42ddab00daf6ee3f1092515d076c8584c
SaffronPage7583 <jpwl2008@gmail.com>
2026-05-14 09:03:33 -0600

    Added test

--- CS30_Space_Game_Jared_&_James/src/Main.java
+++ CS30_Space_Game_Jared_&_James/src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
     public static void main(String[] args) throws Exception {
+        Star proximaCentauri = new Star(134, 1.5, "Proxima Centauri", "Red Dwarf", "Red", 3);
+        Exoplanet proximaCentauriB = new Exoplanet(0.0037, 0.11, "Proxima Centauri B", "Superearth", false, 0, 234.0, "Incredibly Minimal", proximaCentauri);
+        System.out.println(proximaCentauri.basicScan());
+        System.out.println(proximaCentauri.scan());
+        System.out.println(proximaCentauriB.basicScan());
+        System.out.println(proximaCentauriB.scan());
     }
 }
