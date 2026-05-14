2d25180310c27b4942b1f68b4d3a3c388de62775
James Peterson <jpetes08@outlook.com>
2026-05-14 10:20:05 -0600

    merged new to this branch

--- CS30_Space_Game_Jared_&_James/src/Main.java
+++ CS30_Space_Game_Jared_&_James/src/Main.java
@@ -22,15 +22,15 @@ public class Main {
         String input = scanner.nextLine();
 
         System.out.println("You typed: " + input);
-        System.out.println("Press enter to exit...");
-        scanner.nextLine();
-        //ends
-        scanner.close();
+        
         Star proximaCentauri = new Star(134, 1.5, "Proxima Centauri", "Red Dwarf", "Red", 3);
         Exoplanet proximaCentauriB = new Exoplanet(0.0037, 0.11, "Proxima Centauri B", "Superearth", false, 0, 234.0, "Incredibly Minimal", proximaCentauri);
         System.out.println(proximaCentauri.basicScan());
         System.out.println(proximaCentauri.scan());
         System.out.println(proximaCentauriB.basicScan());
         System.out.println(proximaCentauriB.scan());
+        System.out.println("Press enter to exit...");
+        scanner.nextLine();
+        scanner.close();
     }
 }
